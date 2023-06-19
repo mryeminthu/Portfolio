2a9259ff7eeb8937b9975c5171d06f1aa16246c7
mryeminthu <dhammaarchive@hotmail.com>
2023-06-19 15:48:38 -0700

    Add-JS-for-Hamburger-disappears

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-const toggleBtn = document.getElementsByClassName("toggle")[0];
-const fourMenu = document.getElementsByClassName("four-menu")[0];
-toggleBtn.addEventListener("click", ()=> {
-    fourMenu.classList.toggle("active");
-})
+const toggleBtn = document.getElementsByClassName('toggle')[0];
+const fourMenu = document.getElementsByClassName('four-menu')[0];
+toggleBtn.addEventListener('click', () => {
+  fourMenu.classList.toggle('active');
+});
+const crossBtn = document.getElementsByClassName('cross')[0];
+crossBtn.addEventListener('click', () => {
+  fourMenu.classList.remove('active');
+});
